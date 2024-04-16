7f4ac862d09310b480769a6175c426ed923c0b16
Tomas 'toothless' Lastuvka <65524625+th-toothless@users.noreply.github.com>
2024-04-16 08:28:14 +0200

    Gsi cfg for playzone

--- src/detectcfg.js
+++ src/detectcfg.js
@@ -9,11 +9,11 @@ module.exports = {
 		if (result && result.game && fs.existsSync(result.game.path)) {
 			console.info("Found installation in", result.game.path)
 
-			let configPath = path.join(result.game.path, "game", "core", "cfg", "gamestate_integration_boltobserv.cfg")
+			let configPath = path.join(result.game.path, "game", "core", "cfg", "gamestate_integration_observerspectator.cfg")
 
 			if (fs.existsSync(configPath)) {
 				let foundHeader = fs.readFileSync(configPath, "utf8").split("\n")[0]
-				let ownHeader = fs.readFileSync(path.join(__dirname, "gamestate_integration_boltobserv.cfg"), "utf8").split("\n")[0]
+				let ownHeader = fs.readFileSync(path.join(__dirname, "gamestate_integration_observersepctator.cfg"), "utf8").split("\n")[0]
 
 				if (foundHeader != ownHeader) {
 					module.exports.found.push({
@@ -32,8 +32,8 @@ module.exports = {
 	},
 
 	install: (target) => {
-		let template = path.join(__dirname, "gamestate_integration_boltobserv.cfg")
-		let dest = path.join(target, "game", "core", "cfg", "gamestate_integration_boltobserv.cfg")
+		let template = path.join(__dirname, "gamestate_integration_observerspectator.cfg")
+		let dest = path.join(target, "game", "core", "cfg", "gamestate_integration_observerspectator.cfg")
 		fs.copyFileSync(template, dest)
 
 		console.info("Installed config file as", dest)
